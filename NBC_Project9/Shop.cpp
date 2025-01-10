7fb95e0ab2691381d4ecd6ef7619d64b251e815b
fucidin310 <fucidin310@gmail.com>
2025-01-10 18:08:35 +0900

    Update Shop

--- NBC_Project9/Shop.cpp
+++ NBC_Project9/Shop.cpp
@@ -1,5 +1,6 @@
 #include "Shop.h"
 #include "Character.h"
+#include "Item.h"
 
 Shop::Shop()
 {
@@ -7,17 +8,40 @@ Shop::Shop()
 
 void Shop::displayItem()
 {
+	for (int index = 0; index < availableItems.size(); index++) {
+		Item* curItem = availableItems[index];
+		cout << index << "번 아이템 : " << curItem->GetName() << endl;
+
+		//아이템의 가격 주세요...
+	}
 }
 
 void Shop::buyItem(int index, Character* player)
 {
 	if (index >= availableItems.size()) {
+		cout << "ERROR : Shop->buyItem : availableItems의 범위를 벗어났습니다. : " << index << endl;
 		return;
 	}
 
-	//player->
+	//플레이어의 골드 주세요...
+	//플레이어 골드 줄이기 구현 & 골드량 체크
+
+	vector<Item*>& playerInventory = player->getInventory();
+	playerInventory.push_back(availableItems[index]);
+
 }
 
 void Shop::sellItem(int index, Character* player)
 {
+	vector<Item*>& playerInventory = player->getInventory();
+
+	if (index >= playerInventory.size()) {
+		cout << "ERROR : Shop->sellItem : playerInventory의 범위를 벗어났습니다. : " << index << endl;
+		return;
+	}
+
+	//플레이어의 골드 주세요...
+	//플레이어 골드 늘리기 구현
+
+	playerInventory.erase(playerInventory.begin() + index);
 }
