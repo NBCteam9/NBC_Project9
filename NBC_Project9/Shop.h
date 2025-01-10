1297cf0ecdf4faf3793102de040f905e765f10be
fucidin310 <fucidin310@gmail.com>
2025-01-10 17:36:12 +0900

    Update Shop

--- NBC_Project9/Shop.h
+++ NBC_Project9/Shop.h
@@ -1,5 +1,21 @@
 #pragma once
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
 class Shop
-{
+{ 
+private:
+	vector<class Item*> availableItems;
+
+public:
+	Shop();
+
+	void displayItem();
+
+	void buyItem(int index, class Character* player);
+
+	void sellItem(int index, class Character* player);
 };
 
