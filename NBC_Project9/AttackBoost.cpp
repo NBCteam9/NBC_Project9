45aa1e3be3bd023b1138fcb72973630b71227f2a
Menho <82595532+Menho91@users.noreply.github.com>
2025-01-10 17:21:33 +0900

    add : AttackBoost 헤더파일과 cpp 파일 생성, update : AttackBoost 아이템 기능 구현

--- NBC_Project9/AttackBoost.cpp
+++ NBC_Project9/AttackBoost.cpp
@@ -1,1 +1,17 @@
 #include "AttackBoost.h"
+
+AttackBoost::AttackBoost()
+{
+
+}
+
+const string AttackBoost::getName()
+{
+	return name;
+}
+
+void AttackBoost::use(Character* character)
+{
+	character->UseItem(attackIncrease);
+	cout << "공격력이 " << attackIncrease << " 올랐습니다." << endl;
+}
