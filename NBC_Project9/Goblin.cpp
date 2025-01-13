35d6c79c16c29c26a69134842af925ce2df83f04
Roanne <83761585+Roanne0258@users.noreply.github.com>
2025-01-13 15:43:19 +0900

    Removed comments

--- NBC_Project9/Goblin.cpp
+++ NBC_Project9/Goblin.cpp
@@ -1,12 +1,14 @@
 #include "Goblin.h"
-#include <cstdlib> // rand(), srand()
+#include <cstdlib>
 
 Goblin::Goblin(int level) : Monster(level)
 {
 	int randomValue1 = rand() % 10;
 	int randomValue2 = rand() % 5;
+
 	name = "Goblin";
-	health = 60 + level * (20+randomValue1); // 20~30
-	attack = 11 + level * (5+randomValue2);  // 5~10
+	health = 60 + level * (20+randomValue1);
+	attack = 11 + level * (5+randomValue2);
+
 	cout << "Goblin Spawned!!" << endl;
 }
