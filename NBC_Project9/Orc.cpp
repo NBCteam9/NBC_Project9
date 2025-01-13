37b73eece56defbb77269ad636ea57592c0bc5b0
yeong132 <pfyrmog6964@gmail.com>
2025-01-13 10:05:11 +0900

    update Orc 생성자 추가

--- NBC_Project9/Orc.cpp
+++ NBC_Project9/Orc.cpp
@@ -1,1 +1,7 @@
 #include "Orc.h"
+
+
+Orc::Orc(string n, int h, int a) : Monster(n, h, a)
+{
+	cout << "Orc Spawned!!" << endl;
+}
--- NBC_Project9/Orc.h
+++ NBC_Project9/Orc.h
@@ -4,4 +4,6 @@
 class Orc :
     public Monster
 {
+public:
+    Orc(string n, int h, int a);
 };
