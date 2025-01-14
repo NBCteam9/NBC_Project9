a572bed8aafad9d2672de34865b0e481033b75ad
Roanne <83761585+Roanne0258@users.noreply.github.com>
2025-01-14 15:05:38 +0900

    드래곤 밸런스 수정

--- NBC_Project9/Dragon.cpp
+++ NBC_Project9/Dragon.cpp
@@ -7,8 +7,8 @@ Dragon::Dragon(int level) : Monster(level)
 	int goldReward = getRandomNum(100, 300);
 
 	name = "Dragon";
-	health = 120 + level * (60 + healthRandomValue);
-	attack = 15 + level * (12 + attackRandomValue);
+	health = 80 + level * (45 + healthRandomValue);
+	attack = 11 + level * (10 + attackRandomValue);
 	dropRate = 97;
 	dropGold = goldReward;
 }
