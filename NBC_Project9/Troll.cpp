2d7e19c26455a4c57f80819833b0704f78db618e
Roanne <83761585+Roanne0258@users.noreply.github.com>
2025-01-14 12:44:23 +0900

    트롤 너프

--- NBC_Project9/Troll.cpp
+++ NBC_Project9/Troll.cpp
@@ -7,7 +7,7 @@ Troll::Troll(int level) : Monster(level)
 	int goldReward = getRandomNum(20, 25);
 
 	name = "Troll";
-	health = 60 + level * (35 + healthRandomValue);
+	health = 60 + level * (30 + healthRandomValue);
 	attack = 10 + level * (9 + attackRandomValue);
 	dropRate = 40;
 	dropGold = goldReward;
