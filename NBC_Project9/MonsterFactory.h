75e742adc02d66a99a4e687665919840f5aa6d31
Menho <82595532+Menho91@users.noreply.github.com>
2025-01-15 20:23:34 +0900

    fix : MonsterFactory.h 코드 컨벤션

--- NBC_Project9/MonsterFactory.h
+++ NBC_Project9/MonsterFactory.h
@@ -6,20 +6,15 @@ using namespace std;
 
 class MonsterFactory
 {
-private:
-	static MonsterFactory* instance;
-
-	vector <class Monster*> availableMonsters;
-	vector <class Monster*> availableBossMonsters;
-
 public:
 	static MonsterFactory* GetInstance();
-
 	class Monster* GenerateRandomMonster(int level);
 	class Monster* GenerateRandomBossMonster();
-
 	void AddMonster(Monster* monster);
 	void AddBossMonster(Monster* bossMonster);
-
 	~MonsterFactory();
+private:
+	static MonsterFactory* instance;
+	vector <class Monster*> availableMonsters;
+	vector <class Monster*> availableBossMonsters;
 };
